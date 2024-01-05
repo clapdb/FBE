2541cd4e6bf9719355c37f71d00807ce5efd2c2a
liuqi <liuqi@stdb.io>
2024-01-05 20:29:20 +0800

    fix incompatible, seastar::sstring missing assign

--- include/generator_cpp_fixture.h
+++ include/generator_cpp_fixture.h
@@ -6371,7 +6371,11 @@ struct ValueReader<TJson, FBEString>
             return false;
 
         // Save the value
+        #if defined(USING_SEASTAR_STRING)
+        value = json.GetString();
+        #else
         value.assign(json.GetString(), (size_t)json.GetStringLength());
+        #endif
         return true;
     }
 };
